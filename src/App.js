9167c1d5d954b152c37f6940d6ea3e082f2ca77a
l0o0gy <minamahone9620@gmail.com>
2024-09-19 16:48:21 +0300

    remove basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter  basename="/ibrahimportfolio">
+      <BrowserRouter 
+      //  basename="/ibrahimportfolio"
+       >
         <AppBar />
           <Routes>
             <Route path='/' element={<Homepage />} />
