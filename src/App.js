9489ce201e70cd90d6f2b81d4fb70dd25a151c30
l0o0gy <minamahone9620@gmail.com>
2024-09-19 15:54:31 +0300

    add basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter  basename="/ibrahimportfolio">
         <AppBar />
           <Routes>
             <Route path='/' element={<Homepage />} />
