6f0e4ca453405108cafa33eed153cd12fdb7c28f
l0o0gy <minamahone9620@gmail.com>
2024-09-20 02:44:20 +0300

    add links to herosection

--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -7,6 +7,19 @@ import img from '../assets/img/img.png';
 import engname from '../assets/img/engname.png';
 
 function HeroSection() {
+  const handleInstagramClick = () => {
+    window.open('https://www.instagram.com/_.ebo/', 'noopener,noreferrer');
+  };
+
+  // const handleWhatsAppClick = () => {
+  //   window.open('https://api.whatsapp.com/send?phone=647733599572&text=hello', '_blank', 'noopener,noreferrer');
+  // };
+  // <a href="https://api.whatsapp.com/send?phone=647733599572&text=YOUR_MESSAGE">Chat with us on WhatsApp</a>
+  // const sendWhatsAppMessage = () => {
+  //   const link = `https://api.whatsapp.com/send?phone=+964${post.user_number}&text=I'm%20interested%20in%20your%20post%20${post.item_name}`;
+  //   window.open(link, '_blank');
+  // };
+
   return (
     <div>
       <Box style={{
@@ -20,14 +33,14 @@ function HeroSection() {
         <Box>
           <img src={engname} style={{ width: 170, marginTop: 30, marginLeft: '-30px' }} alt="name in English" />
           <Box sx={{ display: 'flex', justifyContent: 'space-evenly', marginLeft: '-40px' }}>
-            <a href="https://www.instagram.com/_.ebo/" target="_blank" rel="noopener noreferrer">
-              <InstagramIcon sx={{ color: 'white' }} />
+            <a href="https://www.instagram.com/_.ebo/">
+            <InstagramIcon sx={{ color: 'white' }} />
             </a>
             <a href="https://t.me/hema_81" target="_blank" rel="noopener noreferrer">
               <TelegramIcon sx={{ color: 'white' }} />
             </a>
-            <a href="https://wa.me/9647733599572" target="_blank" rel="noopener noreferrer">
-              <WhatsAppIcon sx={{ color: 'white' }} />
+            <a href="https://api.whatsapp.com/send?phone=9647733599572&text=Hello">
+            <WhatsAppIcon sx={{ color: 'white' }} />
             </a>
           </Box>
         </Box>
