ada646c3e051d79e2ec6d1fb6b70bca7f3699ccd
l0o0gy <minamahone9620@gmail.com>
2024-09-19 18:46:01 +0300

    clean code

--- src/Page/Homepage.jsx
+++ src/Page/Homepage.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useData } from '../assets/Contacts/store';
+// import { useData } from '../assets/Contacts/store';
 import { Box } from '@mui/material';
+import HeroSection from '../Components/HeroSection';
 
 function Homepage() {
 
   return (
-    <div>
-      <Box>
-
-      </Box>
+    <div style={{background:'red'}}>
+      <HeroSection />
     </div>
   );
 }
