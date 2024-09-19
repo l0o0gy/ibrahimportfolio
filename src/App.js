bdc6fb127ae50905b06a1609c6f4d38034326474
l0o0gy <minamahone9620@gmail.com>
2024-09-19 18:46:14 +0300

    clean code

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Homepage from './Page/Homepage';
 import AppBar from './Components/AppBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
-
+import HeroSection from './Components/HeroSection';
 function App() {
   return (
     <div className="App">
       <BrowserRouter 
-      //  basename="/ibrahimportfolio"
+       basename="/ibrahimportfolio"
        >
         <AppBar />
           <Routes>
-            <Route path='/' element={<Homepage />} />
+            <Route path='/' element={<Homepage/>} />
+            <Route path='herosection' element={<HeroSection/>} />
           </Routes>
       </BrowserRouter>
     </div>
