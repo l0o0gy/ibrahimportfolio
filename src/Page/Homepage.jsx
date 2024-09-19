d0d24d61c5b1ae6e92c8141d2335970744c05f51
l0o0gy <minamahone9620@gmail.com>
2024-09-19 15:51:13 +0300

    add home page

---
+++ src/Page/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useData } from '../assets/Contacts/store';
+import { Box } from '@mui/material';
+
+function Homepage() {
+
+  return (
+    <div>
+      <Box>
+
+      </Box>
+    </div>
+  );
+}
+
+export default Homepage;
