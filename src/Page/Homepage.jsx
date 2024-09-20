ae00263a427de99df9b5fc81a567ab6269af895c
l0o0gy <minamahone9620@gmail.com>
2024-09-20 05:09:23 +0300

    add about compo to homepage

--- src/Page/Homepage.jsx
+++ src/Page/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 // import { useData } from '../assets/Contacts/store';
 import { Box } from '@mui/material';
 import HeroSection from '../Components/HeroSection';
+import AboutSection from '../Components/AboutSection';
 
 function Homepage() {
 
   return (
-    <div style={{background:'red'}}>
+    <div >
       <HeroSection />
+      <AboutSection/>
     </div>
   );
 }
