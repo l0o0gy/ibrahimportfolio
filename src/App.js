bf1430d3bc1bb6e87263f48fd94cc7665f713fae
l0o0gy <minamahone9620@gmail.com>
2024-09-22 01:59:33 +0300

    add work page route to homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Homepage from './Page/Homepage';
 import AppBar from './Components/AppBar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import HeroSection from './Components/HeroSection';
+import ProjectSection from './Components/ProjectSection';
+import Work from './Page/Work';
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +13,9 @@ function App() {
         <AppBar />
           <Routes>
             <Route path='/' element={<Homepage/>} />
-            <Route path='herosection' element={<HeroSection/>} />
+            <Route path='/herosection' element={<HeroSection/>} />
+            <Route path="/projects" element={<ProjectSection />} />
+            <Route path="/project/:projectId" element={<Work />} />
           </Routes>
       </BrowserRouter>
     </div>
