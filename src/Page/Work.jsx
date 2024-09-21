77c46ac80e9c5c4a1c051a5e6dfc82b6334d9034
l0o0gy <minamahone9620@gmail.com>
2024-09-22 02:00:55 +0300

    add work page

---
+++ src/Page/Work.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom';
+
+
+function Work() {
+    const location = useLocation();
+
+  return (
+    <div>
+      <h1>Ibrahim work</h1>
+    </div>
+  )
+}
+
+export default Work
